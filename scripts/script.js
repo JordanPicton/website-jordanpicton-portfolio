01d4ba32479f42e9aea81a51ab8b8f1eddf9c20c
JordanPicton <jordan_picton@hotmail.com>
2026-08-28 12:57:31 +0100

    feat: dynamic changing website update date pulled from github.

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,16 @@ function scrollFunction() {
     var scrolled = (winScroll / height) * 100;
     document.getElementById("jsBar").style.width = scrolled + "%";
 }
+
+// Fetch GitHub Update Date, used in the footer.
+let updatedElement = document.getElementById('web-update');
+
+fetch('https://api.github.com/repos/JordanPicton/website-jordanpicton-portfolio/branches/main').then(response => response.json()).then(data => {
+    const dateObj = new Date(data.commit.commit.committer.date);
+    const formattedDate = dateObj.toLocaleDateString('en-GB', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+    });
+    updatedElement.textContent = formattedDate;
+});
